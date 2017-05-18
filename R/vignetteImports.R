29ac2994e87368a845e75322a3f0804ffa81de9f
ehfhcrc <ehenrich@fredhutch.org>
2017-05-18 13:38:40 -0700

    Updated Imports

---
+++ R/vignetteImports.R
@@ -0,0 +1,15 @@
+# MAIN -----------------------------------------------------------------------
+#' The following are packages only called in vignette and need to be imported
+#'
+#' @import gridExtra grid cowplot metaplot RColorBrewer png ggplot2
+
+initLibs <- function(){
+  library(gridExtra)
+  library(grid)
+  library(cowplot)
+  library(metaplot)
+  library(RColorBrewer)
+  library(png)
+  library(ggplot2)
+}
+
